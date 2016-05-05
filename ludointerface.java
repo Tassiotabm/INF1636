f1b958868cf9d99a212bb529e7418733940b1b9d
Tassio <tabm2010@gmail.com>
2016-05-05 15:44:51 -0300

    Agora com os quadrados brancos pintados

--- ludointerface.java
+++ ludointerface.java
@@ -29,7 +29,12 @@ public class ludointerface extends JFrame {
 				for(j=0;j<3;j++)
 				{
 					Graphics2D gbp = (Graphics2D)g;
-					gbp.setPaint(Color.WHITE);
+					if( (i==1 && j==1) || (i==1 && j==2) || (6 > i && i > 1 && j == 1) )
+						gbp.setPaint(Color.red);
+					else if( (i==13 && j==0) || (i==13 && j==1) || (14 > i && i > 8 && j == 1) )
+						gbp.setPaint(Color.yellow);
+					else
+						gbp.setPaint(Color.WHITE);
 					Rectangle2D gb2dr = new Rectangle2D.Double(120+j*20,i*20,20,20); //coluna linha
 					gbp.fill(gb2dr);
 					g.setColor(Color.black);
@@ -42,7 +47,12 @@ public class ludointerface extends JFrame {
 				for(j=0;j<15;j++)
 				{
 					Graphics2D gbp = (Graphics2D)g;
-					gbp.setPaint(Color.WHITE);
+					if( (i==0 && j==1) || (i==1 && j==1) || (6 > j && j > 1 && i == 1) )
+						gbp.setPaint(Color.green);
+					else if( (i==2 && j==13) || (i==1 && j==13) || (14 > j && j > 8 && i == 1) )
+						gbp.setPaint(Color.blue);
+					else
+						gbp.setPaint(Color.WHITE);
 					Rectangle2D gb2dr = new Rectangle2D.Double(j*20,120+i*20,20,20); //coluna linha
 					gbp.fill(gb2dr);
 					g.setColor(Color.black);
@@ -70,6 +80,17 @@ public class ludointerface extends JFrame {
 			g4.fill(g4r);
 			g.setColor(Color.black);
 			g.drawRect(180,180,120,120);
+			//desenhar fucking triangles
+			Graphics2D t1 = (Graphics2D) g;
+			t1.setPaint(Color.green);
+			
+			
+			/*Path2D.Double triangle = new Path2D.Double();
+triangle.moveTo(x1, y1);
+triangle.pathTo(x2, y2);
+triangle.pathTo(x3, y3);
+triangle.closePath();
+g2d.fill(triangle);*/
 		}
 	}
 	private void initUI(){
