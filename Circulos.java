169973833322bd53872a88c44587acbaaf6e74bb
Tassio <tabm2010@gmail.com>
2016-06-01 00:57:25 -0300

    circulos sendo impressos do Quadrado

---
+++ Circulos.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+public class Circulos {
+	
+	// Desenhar os Círculos Verdes
+	private int x;
+	private int y;
+	
+	public Circulos(int x1,int y1,Graphics G,String cor)
+	{
+		x = x1;
+		y = y1;
+		
+		Graphics2D g = (Graphics2D) G;
+		
+		if(cor == "G"){ // Green Big
+			Graphics2D gEllipseGreen=(Graphics2D) g;
+			gEllipseGreen.setPaint(Color.green);
+			Ellipse2D eg1 = new Ellipse2D.Double(x1,y1,30,30);
+			gEllipseGreen.fill(eg1);
+			g.setColor(Color.black);
+			g.drawOval(x1,y1,30,30);
+		}
+		else if(cor == "R"){ // Red Big
+			Graphics2D gEllipseRed=(Graphics2D) g;
+			gEllipseRed.setPaint(Color.red);
+			Ellipse2D er1 = new Ellipse2D.Double(x1,y1,30,30);
+			gEllipseRed.fill(er1);
+			g.setColor(Color.black);
+			g.drawOval(x1,y1,30,30);
+		}
+		else if(cor == "S"){ // Black
+			Graphics2D gSmall=(Graphics2D) g;
+			gSmall.setPaint(Color.black);
+			Ellipse2D eg2 = new Ellipse2D.Double(x1,y1,24,24);
+			gSmall.fill(eg2);
+			g.setColor(Color.black);
+			g.drawOval(x1,y1,24,24);
+		}
+		else if(cor == "Y"){
+			Graphics2D gEllipseYellow=(Graphics2D) g;
+			gEllipseYellow.setPaint(Color.yellow);
+			Ellipse2D ey1 = new Ellipse2D.Double(x1,y1,30,30);
+			gEllipseYellow.fill(ey1);
+			g.setColor(Color.black);
+			g.drawOval(x1,y1,30,30);
+		}
+		else{ // cor == "B"
+			Graphics2D gEllipseBlue=(Graphics2D) g;
+			gEllipseBlue.setPaint(Color.blue);
+			Ellipse2D eb1 = new Ellipse2D.Double(x1,y1,30,30);
+			gEllipseBlue.fill(eb1);
+			g.setColor(Color.black);
+			g.drawOval(x1,y1,30,30);
+		}
+
+
+	}
+}
--- QuadradoGrande.java
+++ QuadradoGrande.java
@@ -20,22 +20,54 @@ public class QuadradoGrande {
 			Rectangle2D g2 =new Rectangle2D.Double(x1,y1,120,120);				
 			g.fill(g2);
 			G.setColor(Color.black);
+			Circulos Red1Big = new Circulos(195,15,g,"R");
+			Circulos Red1Small = new Circulos(198,18,g,"S");
+			Circulos Red2Big = new Circulos(255,15,g,"R");
+			Circulos Red2Small = new Circulos(258,18,g,"S");
+			Circulos Red3Big = new Circulos(195,75,g,"R");
+			Circulos Red3Small = new Circulos(198,78,g,"S");
+			Circulos Red4Big = new Circulos(255,75,g,"R");
+			Circulos Red4Small = new Circulos(258,78,g,"S");
 			}
 		else if(cor == "yellow"){
 			g.setPaint(Color.yellow);
 			Rectangle2D g2 =new Rectangle2D.Double(x1,y1,120,120);				
 			g.fill(g2);
-			G.setColor(Color.black);}
+			G.setColor(Color.black);
+			Circulos Yellow1Big = new Circulos(15,195,g,"Y");
+			Circulos Yellow1Small = new Circulos(18,198,g,"S");
+			Circulos Yellow2Big = new Circulos(75,195,g,"Y");
+			Circulos Yellow2Small = new Circulos(78,198,g,"S");
+			Circulos Yellow3Big = new Circulos(15,255,g,"Y");
+			Circulos Yellow3Small = new Circulos(18,258,g,"S");
+			Circulos Yellow4Big = new Circulos(75,255,g,"Y");
+			Circulos Yellow4Small = new Circulos(78,258,g,"S");}
 		else if(cor == "blue"){
 			g.setPaint(Color.blue);
 			Rectangle2D g2 =new Rectangle2D.Double(x1,y1,120,120);				
 			g.fill(g2);
-			G.setColor(Color.black);}
+			G.setColor(Color.black);
+			Circulos Blue1Big = new Circulos(195,195,g,"B");
+			Circulos Blue1Small = new Circulos(198,198,g,"S");
+			Circulos Blue2Big = new Circulos(255,195,g,"B");
+			Circulos Blue2Small = new Circulos(258,198,g,"S");
+			Circulos Blue3Big = new Circulos(195,255,g,"B");
+			Circulos Blue3Small = new Circulos(198,258,g,"S");
+			Circulos Blue4Big = new Circulos(255,255,g,"B");
+			Circulos Blue4Small = new Circulos(258,258,g,"S");}
 		else{ //verde
 			g.setPaint(Color.green);
 			Rectangle2D g2 =new Rectangle2D.Double(x1,y1,120,120);				
 			g.fill(g2);
 			G.setColor(Color.black);
+			Circulos Green1Big = new Circulos(15,15,g,"G");
+			Circulos Green1Small = new Circulos(18,18,g,"S");
+			Circulos Green2Big = new Circulos(75,15,g,"G");
+			Circulos Green2Small = new Circulos(78,18,g,"S");
+			Circulos Green3Big = new Circulos(15,75,g,"G");
+			Circulos Green3Small = new Circulos(18,78,g,"S");
+			Circulos Green4Big = new Circulos(75,75,g,"G");
+			Circulos Green4Small = new Circulos(78,78,g,"S");
 		}
 		G.drawRect(x1,y1,120,120);
 	}
