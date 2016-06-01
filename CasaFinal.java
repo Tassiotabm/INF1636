534a7c30a22117e0944a1fd1ee1e137decdf7a8f
Felipe VieiraCortes <felipevieiracortes@gmail.com>
2016-06-01 20:18:54 -0300

    Home and Tolkien

---
+++ CasaFinal.java
@@ -0,0 +1,180 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+
+public class CasaFinal {
+	
+	private String cor;
+	private int qtdToken;
+
+	public CasaFinal(String Cor) {
+		this.cor = Cor;
+		this.qtdToken = 0;
+	}
+	public boolean addToken(){
+		if(qtdToken == 4){
+			System.out.println("Casa Final cheia, nao ha como adicionar mais tokens");
+			return false;
+		}
+		else{
+		qtdToken ++;
+		return true;
+		}
+	}
+	public void print(Graphics g){
+		switch(this.cor){
+		case "Verde" :
+		// pintar triângulo 1 Green
+				Graphics2D t1 = (Graphics2D) g;
+				t1.setPaint(Color.green);
+				Path2D.Double triangle = new Path2D.Double();
+				triangle.moveTo(120, 120);
+				triangle.lineTo(120, 180);
+				triangle.lineTo(150, 150);
+				triangle.closePath();
+				t1.fill(triangle);
+				if(this.qtdToken == 1){
+					// uma bolinha no centro
+					g.setColor(Color.black);
+					g.drawOval(125,145,10,10);
+				} else if(this.qtdToken == 2){
+					//primeira bolinha
+					g.drawOval(125,140,10,10);
+					// segunda bolinha
+					g.drawOval(125,150,10,10);
+				}	else if(this.qtdToken == 3){
+					//primeira bolinha
+					g.drawOval(120,140,10,10);
+					// segunda bolinha
+					g.drawOval(120,150,10,10);
+					// terceira bolinha
+					g.drawOval(130,145,10,10);
+				}else if(qtdToken == 4){
+					//primeira bolinha
+					g.drawOval(120,140,10,10);
+					// segunda bolinha
+					g.drawOval(120,150,10,10);
+					// terceira bolinha
+					g.drawOval(130,140,10,10);
+					// quarta bolinha
+					g.drawOval(130,150,10,10);
+				}
+				break;
+		case "Vermelho" :
+				// pintar triângulo 2 Red
+				Graphics2D t2 = (Graphics2D) g;
+				t2.setPaint(Color.red);
+				Path2D.Double triangle2 = new Path2D.Double();
+				triangle2.moveTo(120, 120);
+				triangle2.lineTo(180, 120);
+				triangle2.lineTo(150, 150);
+				triangle2.closePath();
+				t2.fill(triangle2);
+				g.setColor(Color.black);
+				if(this.qtdToken == 1){
+					// uma bolinha no centro
+					g.drawOval(145,125,10,10);
+				} else if(this.qtdToken == 2){
+					//primeira bolinha
+					g.drawOval(140,125,10,10);
+					// segunda bolinha
+					g.drawOval(150,125,10,10);
+				}	else if(this.qtdToken == 3){
+					//primeira bolinha
+					g.drawOval(140,120,10,10);
+					// segunda bolinha
+					g.drawOval(150,120,10,10);
+					// terceira bolinha
+					g.drawOval(145,130,10,10);
+				}else if(this.qtdToken == 4){
+					//primeira bolinha
+					g.drawOval(140,120,10,10);
+					// segunda bolinha
+					g.drawOval(150,120,10,10);
+					// terceira bolinha
+					g.drawOval(140,130,10,10);
+					// quarta bolinha
+					g.drawOval(150,130,10,10);
+				}
+				break;
+		case "Azul" : 
+				// pintar triângulo 3 Blue
+				Graphics2D t3 = (Graphics2D) g;
+				t3.setPaint(Color.blue);
+				Path2D.Double triangle3 = new Path2D.Double();
+				triangle3.moveTo(180, 120);
+				triangle3.lineTo(180, 180);
+				triangle3.lineTo(150, 150);
+				triangle3.closePath();
+				t3.fill(triangle3);
+				g.setColor(Color.black);
+				if(this.qtdToken == 1){
+					// uma bolinha no centro
+					g.drawOval(165,145,10,10);
+				} else if(this.qtdToken == 2){
+					//primeira bolinha
+					g.drawOval(165,140,10,10);
+					// segunda bolinha
+					g.drawOval(165,150,10,10);
+				}	else if(this.qtdToken == 3){
+					//primeira bolinha
+					g.drawOval(170,140,10,10);
+					// segunda bolinha
+					g.drawOval(170,150,10,10);
+					// terceira bolinha
+					g.drawOval(160,145,10,10);
+				}else if(this.qtdToken == 4){
+					//primeira bolinha
+					g.drawOval(160,140,10,10);
+					// segunda bolinha
+					g.drawOval(160,150,10,10);
+					// terceira bolinha
+					g.drawOval(170,140,10,10);
+					// quarta bolinha
+					g.drawOval(170,150,10,10);
+				}
+				break;
+		case "Amarelo" : 
+				// pintar triângulo 4 Yellow
+				Graphics2D t4 = (Graphics2D) g;
+				t4.setPaint(Color.yellow);
+				Path2D.Double triangle4 = new Path2D.Double();
+				triangle4.moveTo(120, 180);
+				triangle4.lineTo(180, 180);
+				triangle4.lineTo(150, 150);
+				triangle4.closePath();
+				t4.fill(triangle4);
+				g.setColor(Color.black);
+				if(this.qtdToken == 1){
+					// uma bolinha no centro
+					g.drawOval(145,165,10,10);
+				} else if(this.qtdToken == 2){
+					//primeira bolinha
+					g.drawOval(140,165,10,10);
+					// segunda bolinha
+					g.drawOval(150,165,10,10);
+				}	else if(this.qtdToken == 3){
+					//primeira bolinha
+					g.drawOval(140,170,10,10);
+					// segunda bolinha
+					g.drawOval(150,170,10,10);
+					// terceira bolinha
+					g.drawOval(145,160,10,10);
+				}else if(this.qtdToken == 4){
+					//primeira bolinha
+					g.drawOval(140,160,10,10);
+					// segunda bolinha
+					g.drawOval(150,160,10,10);
+					// terceira bolinha
+					g.drawOval(140,170,10,10);
+					// quarta bolinha
+					g.drawOval(150,170,10,10);
+				}
+				break;
+				
+		default : ;
+		}
+	}
+}
--- Token.java
+++ Token.java
@@ -1,49 +1,75 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.Ellipse2D;
 
 import javax.swing.ImageIcon;
 
 public class Token {
 	private String cor;
-	ImageIcon token;
+	private int position;
+	// Trajetos estáticos, são os mesmos para cada peça
+	static Trajeto greenpath = new Trajeto("Verde");
+	static Trajeto redpath = new Trajeto("Vermelho");
+	static Trajeto bluepath = new Trajeto("Azul");
+	static Trajeto yellowpath = new Trajeto("Amarelo");
 	public Token(String Cor){
 		this.cor = Cor;
-		Image image;
-		Image newimg;
-		switch (Cor){
-		case "Vermelho":
-			token = new ImageIcon("images/redToken.jpg");
-			image = token.getImage(); // transform it 
-			newimg = image.getScaledInstance(19, 19,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-			token = new ImageIcon(newimg);  // transform it back
-			break;
-		case "Verde" :
-			token = new ImageIcon("images/greenToken.jpg");
-			image = token.getImage(); // transform it 
-			newimg = image.getScaledInstance(19, 19,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-			token = new ImageIcon(newimg);
-			break;
-		case "Amarelo":
-			token = new ImageIcon("images/yellowToken.jpg");
-			image = token.getImage(); // transform it 
-			newimg = image.getScaledInstance(19, 19,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-			token = new ImageIcon(newimg);
-			break;
-		case "Azul" :
-			token = new ImageIcon("images/blueToken.jpg");
-			image = token.getImage(); // transform it 
-			newimg = image.getScaledInstance(19, 19,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-			token = new ImageIcon(newimg);
-			break;
-		default:
-			token = new ImageIcon("images/blackToken.jpg");
-			image = token.getImage(); // transform it 
-			newimg = image.getScaledInstance(19, 19,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-			token = new ImageIcon(newimg);
-			break;
-		}
+		this.position = 0;
 	}
 	public Token(){
-		this.cor = "Desprovida";
-		token = new ImageIcon("images/blackToken.jpg");
+		this.cor = "Preto";
+		this.position = -1;
+	}
+	void move(int pos){
+		this.position += pos;
+	}
+
+	public void printToken(Graphics G){
+		switch (this.cor){
+		case "Verde" :
+		Graphics2D gEllipseGreen=(Graphics2D) G;
+		gEllipseGreen.setPaint(Color.green);
+		Ellipse2D eg1 = new Ellipse2D.Double(greenpath.path.get(this.position).x,
+				greenpath.path.get(this.position).y,20,20);
+		gEllipseGreen.fill(eg1);
+		G.setColor(Color.black);
+		G.drawOval(greenpath.path.get(this.position).x,
+				greenpath.path.get(this.position).y,20,20);
+		break;
+		case "Vermelho" :
+			Graphics2D gEllipseRed=(Graphics2D) G;
+			gEllipseRed.setPaint(Color.red);
+			Ellipse2D eg2 = new Ellipse2D.Double(redpath.path.get(this.position).x,
+					redpath.path.get(this.position).y,20,20);
+			gEllipseRed.fill(eg2);
+			G.setColor(Color.black);
+			G.drawOval(redpath.path.get(this.position).x,
+					redpath.path.get(this.position).y,20,20);
+			break;
+		case "Amarelo" : 
+			Graphics2D gEllipseYellow=(Graphics2D) G;
+			gEllipseYellow.setPaint(Color.yellow);
+			Ellipse2D eg3 = new Ellipse2D.Double(yellowpath.path.get(this.position).x,
+					yellowpath.path.get(this.position).y,20,20);
+			gEllipseYellow.fill(eg3);
+			G.setColor(Color.black);
+			G.drawOval(yellowpath.path.get(this.position).x,
+					yellowpath.path.get(this.position).y,20,20);
+			break;
+		case "Azul" : 
+			Graphics2D gEllipseBlue=(Graphics2D) G;
+			gEllipseBlue.setPaint(Color.blue);
+			Ellipse2D eg4 = new Ellipse2D.Double(bluepath.path.get(this.position).x,
+					bluepath.path.get(this.position).y,20,20);
+			gEllipseBlue.fill(eg4);
+			G.setColor(Color.black);
+			G.drawOval(bluepath.path.get(this.position).x,
+					bluepath.path.get(this.position).y,20,20);
+			break;
+			
+			default: ;
+		}
 	}
 }
